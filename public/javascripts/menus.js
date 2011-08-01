1ec1e737b0fbcd0e7159e9e277cdb85337b256ed
Munir Ahmad <munir.ehmad@gmail.com>
2011-08-01 21:32:31 +0500

    Finished Javascript for food dialog, improved some styling

--- public/javascripts/menus.js
+++ public/javascripts/menus.js
@@ -14,57 +14,80 @@ function display_menu(id)
 
 function show_menu_item(id)
 {
-	//alert(id);
+	//alert( 'About to show dialog:' + id );
 
-	dialog_name = 'food_dialog_' + id;
+	dialog = $('#food_dialog_' + id);
 
-	$('#'+dialog_name).show();
+	$('input#total_price',dialog).attr('class','active_price');
+	dialog.show();
+}
+
+function hide_food_dialog(id)
+{
+	//alert( 'About to hide dialog:' + id );
+	var dialog = $('#'+id);
+
+	$('input.active_price',dialog).attr('class','unactive');
+	dialog.hide();
 }
 
 // Food Dialog Bindings
 $( function()
 {
-//binding radio buttons to calculate price
-$(".sub_option").each(function()
-{
-	$(this).unbind('click');
-	var price = $(this).attr("price");
-
-	$(this).click( function()
+	//binding radio buttons to calculate price
+	$(".sub_option").each(function()
 	{
-		var selected_id = $("input:hidden", $(this).parent());
-		var already_added = 0;
+		//$(this).unbind('click');
+		var price = $(this).attr("price");
 
-		if( selected_id.val() != 'nil' )
+		$(this).click( function()
 		{
-			already_added = $("#"+selected_id.val()).attr("price");
-		} 
+			var selected_id = $("input:hidden", $(this).parent());
+			var already_added = 0;
 
-		selected_id.val(this.id);
-		$("#total_price_active").val( parseFloat($("#total_price_active").val()) + parseFloat(price) - parseFloat(already_added) );
+			if( selected_id.val() != 'nil' )
+			{
+				already_added = $("#"+selected_id.val()).attr("price");
+			} 
+
+			selected_id.val(this.id);
+			$("input.active_price").val( parseFloat($("input.active_price").val()) + parseFloat(price) - parseFloat(already_added) );
+		});
+
 	});
 
-});
 
 
-
-		//binding checkboxes to show hide div
-		$('.item_options').each( function( i )
+	//binding checkboxes to show hide div
+	$('.item_options').each( function( i )
+		{
+		$(this).click( function(e)
 			{
-			$(this).click( function(e)
-				{
-				var div = $('#child_opts_' + $(this).attr('id'));
-				var open = this.checked;
-				if(! open){
-				$("input:radio", div).each(
-					function(i){
-					this.checked = false;
-					});     
-				}
-				div.slideToggle();
-				});
+			var div = $('#child_opts_' + $(this).attr('id'));
+			var open = this.checked;
+			if(!open)
+			{
+				var added_item = $('input:hidden',div);
+				var already_added_price = 0;
 
+				if ( added_item.val() != 'nil' )
+				{
+					already_added_price = $('#'+added_item.val()).attr('price');
+					added_item.val('nil');
+				}
+
+				// since we are unchecking all radio boxes we have to reset the price added
+				$("input.active_price").val( parseFloat($("input.active_price").val()) - parseFloat(already_added_price) );
+					
+				$("input:radio", div).each( function()
+				{
+					this.checked = false;
+				});     
+			}
+			div.slideToggle();
 			});
 
 		});
+
+});
 
