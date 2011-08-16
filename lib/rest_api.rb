f06ce5b11c944c252cf1b48a904e86d764c2426d
Abid Mahmood <mabid.mahmood@gmail.com>
2011-08-16 16:25:36 +0500

    more of the order api

--- lib/rest_api.rb
+++ lib/rest_api.rb
@@ -23,24 +23,23 @@ class RestApi
 		self.do_post(url, options)
 	end
 
-
-	protected
-
 	def self.order_hash(order)
 		{ :tray => order.tray, :tip => 0,	:delivery_date => "ASAP", :first_name => User.find(order.user_id).name,
 			:last_name => User.find(order.user_id).name, :addr => order.location.street, :city => order.location.city,
-			:state => order.location.state, :zip => order.location.zip, :phone => "103337896543", :em => "aabaassit@gmail.com",
+			:state => order.location.state, :zip => order.location.zip.split("-")[0], :phone => "103337896543", :em => "aabaassit@gmail.com",
 			:card_name => order.card.name, :card_number => order.card.number, :card_cvv => order.card.cvv,
 			:card_expirty => order.card.expiry, :card_bill_addr => order.card.bill_addr, :card_bill_addr2 => order.card.bill_addr2,
-			:card_bill_city => order.card.bill_city, :card_bill_state => order.card.bill_state, :card_bill_zip => order.card.bill_zip}
+			:card_bill_city => order.card.bill_city, :card_bill_state => order.card.bill_state, :card_bill_zip => order.card.bill_zip.split("-")[0]}
 	end
 
-	
+	protected
+
 	def self.add_auth_params(url)
 		url+"?"+self.auth_params
 	end
 
 	def self.do_get(url)
+		puts URI.escape( add_auth_params url ).inspect
 		get(URI.escape( add_auth_params url )).parsed_response
 	end
 
